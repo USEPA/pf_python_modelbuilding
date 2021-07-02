71b0b6b2a0a2d0d47d962b83746d3f6f4a5951af
AA\GSINCL01 <sinclair.gabriel@epa.gov>
2021-07-02 14:13:13 -0400

    Added Christian's updated DNN with documentation

--- models/dnn_model.py
+++ models/dnn_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, BatchNormalization, Activation
 from sklearn.metrics import r2_score
 from sklearn.metrics import balanced_accuracy_score
 import shutil  #used for deleting model in temp directory once predictions are made
@@ -10,10 +11,11 @@ from keras import models
 from keras import regularizers
 import json
 import os
+import time
 from models import df_utilities as dfu
 
-
-# if you specify a test dataset, you probably want only test returned.
+# technically a redundant function that is handled in the DFU.prepareinstances method but I have noticed my models are much worse if I don't send train_features and pred_features
+# through. Normalizes the data and returns test data based on the mean and standard deviation determined by the train data.
 def normalize(train_pandas, test_pandas):
     mean = train_pandas.mean(axis=0)
     train_pandas -= mean
@@ -29,19 +31,11 @@ def normalize(train_pandas, test_pandas):
         return train_pandas
 
 
-def trim_correlated(train_pandas, test_pandas, threshold):
-    df_corr = train_pandas.corr(method='pearson', min_periods=1)
-    df_not_correlated = ~(df_corr.mask(np.tril(np.ones([len(df_corr)] * 2, dtype=bool))).abs() > threshold).any()
-    un_corr_idx = df_not_correlated.loc[df_not_correlated[df_not_correlated.index] == True].index
-    df_out = train_pandas[un_corr_idx]
-    if test_pandas is not None:
-        df_test_out = test_pandas[un_corr_idx]
-        return df_out, df_test_out
-    else:
-        return df_out
-
-
 # this has to be done because I can't pickle the tensorflow model files, can only do so for the compressed model files
+# this class describes everything about a folder directory that tensorflow uses to store model file binaries about weights, biases, etc.
+# unlike random forest where in the model class self.rfr equals whatever the model object is, I have to set its Model.model_files_objects
+# equal to these classes, these classes are capable of picking up everything in tensorflow model folders, and also recreating those
+# folders whenever a prediction is made
 class ModelFiles:
     def __init__(self, path):
         self.assetfolder_string = "assets"
@@ -60,12 +54,15 @@ class ModelFiles:
         self.variables_DATA_00000_OF_00001 = self.openandclose(path, self.variablefolder_string,
                                                                self.variables_DATA_00000_OF_00001_filename)
 
+# opens and closes files and saves the content in the ModelFiles object
     def openandclose(self, path, subfolder, filename):
         f = open(path + "/" + subfolder + "/" + filename, "rb")
         file_content = f.read()
         f.close()
         return file_content
 
+# this method is used whenever binaries for models are sent through the web service (like when only a prediction request is made)
+# all the binaries are unpacked and tensorflow is able to use the folders it recognizes (like model0 - model4) to reconstruct the models
     def create_model_directory_from_binary(self, parent_dir, modelnum):
         modelnumdir_string = "model" + str(modelnum)
         self.destination_path = parent_dir + "/" +modelnumdir_string
@@ -87,6 +84,7 @@ class ModelFiles:
         DATA_File = open(self.destination_variables_subpath + "/" + self.variables_DATA_00000_OF_00001_filename, "wb")
         DATA_File.write(self.variables_DATA_00000_OF_00001)
 
+# model class that houses most information about the model tuning.
 class Model:
     def __init__(self, df_training, remove_log_p_descriptors):
         self.model_files_objects = None
@@ -95,40 +93,47 @@ class Model:
         self.df_training = df_training
         self.is_binary = None
         self.temp_path = os.path.join(os.getcwd(), "temp")
-        self.epochs = 150
+        self.epochs = 300
+        self.batch = 32
         self.k = 5
-        self.version = "1.7"
+        self.version = "1.8"
         self.remove_corr = True
         self.corr_threshold = 0.95
         self.un_corr_idx = None
         self.qsar_method = 'Deep Neural Network'
         self.description = 'keras implementation of DNN'
 
+    # model building method that calls the k-fold cross validation on the data output from prepare instances
+    # first of two calls to normalize that I need to figure out how to remove at some point.
     def build_model(self):
         train_ids, train_labels, train_features, train_column_names, self.is_binary = \
             dfu.prepare_instances(self.df_training, "training", self.remove_log_p_descriptors, self.remove_corr)
         self.descriptor_names = train_column_names
         train_features_pandas = pd.DataFrame(train_features)
-        self.un_corr_idx = train_features_pandas.index
         normalized_features_pandas = normalize(train_features_pandas, None)
         normalized_features_np = np.array(normalized_features_pandas)
         self.kxfoldvalidation(normalized_features_np, train_labels)
         return self
 
-
+    # this is the method called for continuous models, taking in the train and test data as well as the index of the fold in k-fold cross validation (k)
     def kerasmodel(self, train_data_np, test_data_np, index):
         model = models.Sequential()
-        model.add(layers.Dense(144, activation='relu', kernel_regularizer=regularizers.l1(0.003),
-                               input_shape=(train_data_np.shape[1],))) # 144
-        # model.add(layers.Dropout(0.5))
-        # model.add(BatchNormalization())
-        model.add(layers.Dense(81, kernel_regularizer=regularizers.l1(0.003), activation='relu')) # 81
-        model.add(layers.Dense(36, activation='relu'))
-        model.add(layers.Dense(18, activation='relu'))
-        model.add(layers.Dense(9, activation='relu'))
+        #chunk 1
+        model.add(layers.Dense(120, kernel_regularizer=regularizers.l1(0.001),
+                               input_shape=(train_data_np.shape[1],)))
+        model.add(Activation("relu"))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.5))
+        #chunk 2
+        model.add(layers.Dense(60))
+        model.add(Activation("relu"))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.5))
+        #chunk 3
+
         model.add(layers.Dense(1))
         model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-        model.fit(train_data_np, test_data_np, epochs=self.epochs, batch_size=1, verbose=0)
+        model.fit(train_data_np, test_data_np, epochs=self.epochs, batch_size=self.batch, verbose=0)
         model.save(self.temp_path + "/model" + str(index))
 
         stringlist = []
@@ -140,6 +145,37 @@ class Model:
         tf.keras.backend.clear_session()
         return model
 
+    # the settings for the categorical model, turns out copying the network structure of the continuous model works fine.
+    # Different loss, different metrics for evaluation when training than continuous, but very similar.
+    def categorical_model(self, train_data_np, test_data_np, index):
+        model = models.Sequential()
+        #chunk 1
+        model.add(layers.Dense(120, kernel_regularizer=regularizers.l1(0.001),
+                               input_shape=(train_data_np.shape[1],)))
+        model.add(Activation("relu"))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.5))
+        #chunk 2
+        model.add(layers.Dense(60))
+        model.add(Activation("relu"))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dropout(0.5))
+        #chunk 3
+
+        model.add(layers.Dense(1))
+
+        model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+        model.fit(train_data_np, test_data_np, epochs=self.epochs, batch_size=self.batch, verbose=0)
+        model.save(self.temp_path + "/model" + str(index))
+        tf.keras.backend.clear_session() # I'm not sure this does anything
+        return model
+
+
+
+    # k-fold cross validation method that creates multiple models depending on the number of folds
+    # parameters are the features and target numpy arrays, adds model files to the Model class as a list depending on the number of folds k.
+    # does the validation/train splitting based on the size of the dataset
+    # is endpoint specific (binary/continuous)
     def kxfoldvalidation(self, features_np, targets_np):
         k = self.k
         num_validation_samples = len(features_np) // k
@@ -198,19 +234,8 @@ class Model:
 
         print("all scores accuracy", all_scores_accuracy)
 
-
-    def categorical_model(self, train_data_np, test_data_np, index):
-        model = models.Sequential()
-        model.add(layers.Dense(400, activation='relu', kernel_regularizer=regularizers.l1(0.001),
-                               input_shape=(train_data_np.shape[1],)))
-        model.add(layers.Dense(200, activation='relu', kernel_regularizer=regularizers.l1(0.001)))
-        model.add(layers.Dense(1, activation='sigmoid'))
-        model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-        model.fit(train_data_np, test_data_np, epochs=self.epochs, batch_size=1, verbose=0)
-        model.save(self.temp_path + "/model" + str(index))
-        tf.keras.backend.clear_session() # I'm not sure this does anything
-        return model
-
+    # does the predictions on the prediction dataframe taken in as a parameter. cycles through all the models, generates predictions, and averages those as final predictions
+    # after predictions are generated, does some console output of performance based on binary status.
     def do_predictions(self, df_prediction):
         train_ids, train_labels, train_features, train_column_names, isbinary = \
             dfu.prepare_instances(self.df_training, "training", self.remove_log_p_descriptors, self.remove_corr)
@@ -231,12 +256,13 @@ class Model:
         avg_predictions = np.mean(prediction_np, axis=0)
         if self.is_binary is not True:
             r2 = r2_score(pred_labels, avg_predictions)
-            print('Rsq for Test data = ', r2)
+            print('Rsq for Test Data = ', r2)
         else:
             avg_predictions_binary = np.where(avg_predictions > 0.5, 1, 0)
             BA = balanced_accuracy_score(pred_labels, avg_predictions_binary)
-            print('Balanced Accuracy for Test data =', BA)
+            print('Balanced Accuracy for Test Data =', BA)
 
+        # remove the folder files
         shutil.rmtree(self.temp_path)
 
         return avg_predictions
@@ -246,10 +272,11 @@ class Model:
             Modelfiles_i = self.model_files_objects[i]
             Modelfiles_i.create_model_directory_from_binary(model_dropoff_path, i)
 
-
+# class for the model description
 class ModelDescription:
     def __init__(self, Model):
         self.is_binary = Model.is_binary
+        self.version = Model.version
         self.remove_log_p_descriptors = Model.remove_log_p_descriptors
         self.version = Model.version
         self.qsar_method = Model.qsar_method
@@ -257,28 +284,30 @@ class ModelDescription:
         self.corr_threshold = Model.corr_threshold
         self.n_folds = Model.k
         self.epochs = Model.epochs
+        self.batches = Model.batch
 
-
+    # converts the model description to json format to be stored in the database
     def to_json(self):
         return json.dumps(self.__dict__)
 
-
+# tracks how long the model takes to train and run on tsv files wherever those are in the path.
 def main():
-    df_training = pd.read_csv("LogBCF OPERA T.E.S.T. 5.1 training.tsv", delimiter='\t')
-    df_prediction = pd.read_csv("LogBCF OPERA T.E.S.T. 5.1 prediction.tsv", delimiter='\t')
-    # df_training = pd.read_csv("E:\datasets_benchmark_toxicity\Mutagenicity\Mutagenicity_prediction_set-2d.csv", quotechar='"')
-    # df_prediction = pd.read_csv("E:\datasets_benchmark_toxicity\Mutagenicity\Mutagenicity_training_set-2d.csv", quotechar='"')
+    startTime = time.time()
+
+    # df_training = pd.read_csv("E:\OPERA benchmark sets\LogHalfLife OPERA\LogHalfLife OPERA T.E.S.T. 5.1 training.tsv", delimiter='\t')
+    # df_prediction = pd.read_csv("E:\OPERA benchmark sets\LogHalfLife OPERA\LogHalfLife OPERA T.E.S.T. 5.1 prediction.tsv", delimiter='\t')
+    df_training = pd.read_csv("E:\DataSetsBenchmarkTEST_Toxicity\Mutagenicity\Mutagenicity_prediction_set-2d.csv", quotechar='"')
+    df_prediction = pd.read_csv("E:\DataSetsBenchmarkTEST_Toxicity\Mutagenicity\Mutagenicity_training_set-2d.csv", quotechar='"')
+
     remove_log_p_descriptors = False
+
     model = Model(df_training, remove_log_p_descriptors)
     model.build_model()
-    predictions = model.do_predictions(df_prediction)
+    model.do_predictions(df_prediction)
     print(ModelDescription(model).to_json())
+    executionTime = (time.time() - startTime)
+    print('Execution time in seconds: ' + str(executionTime))
 
 
 if __name__ == "__main__":
     main()
-    # path = "C:/Users/Weeb/Documents/python-qsar-ws/models/model0"
-    # Modelfile0 = ModelFiles(path)
-    # dropoffpath = "E:\models"
-    # for i in range(5):
-    #    Modelfile0.create_model_directory_from_binary(dropoffpath, i)
