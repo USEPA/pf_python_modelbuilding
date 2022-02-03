6e937f45909292702b76ae7c7a3bf906753eeb9a
AA\CRAMSLAN <ramsland.christian@epa.gov>
2022-02-03 15:58:43 -0500

    fixes a syntax error

--- models/dnn_model.py
+++ models/dnn_model.py
@@ -259,7 +259,7 @@ class Model:
         pred_ids, pred_labels, pred_features = DFU.prepare_prediction_instances(df_prediction, train_column_names)
         train_features_pandas = pd.DataFrame(train_features)
         pred_features_pandas = pd.DataFrame(pred_features)
-        print("prediction features size=" pred_features_pandas.shape)
+        print("prediction features size= ", pred_features_pandas.shape)
         normalized_test_features_pandas = normalize(train_features_pandas, pred_features_pandas)
         normalized_test_features_np = np.array(normalized_test_features_pandas)
 
