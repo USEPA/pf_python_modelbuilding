f5a90cc0ea8403e7f39218c58a2afb312aabbb08
AA\TMARTI02 <martin.todd@epa.gov>
2022-02-15 14:46:00 -0500

    Tuning just number of features to consider at every split and the number of decision trees, training r2 is still 0.979 for OPERA logBCF set

--- models/rf_model2.py
+++ models/rf_model2.py
@@ -49,25 +49,36 @@ class Model:
         self.descriptor_names = train_column_names
 
         # Number of trees in random forest
-        n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
+        n_estimators = [int(x) for x in np.linspace(start=100, stop=1000, num=10)]
+
         # Number of features to consider at every split
         max_features = ['sqrt']
+        # max_features = [0.01]
+
         # Maximum number of levels in tree
-        max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-        max_depth.append(None)
-        # Minimum number of samples required to split a node
-        min_samples_split = [2, 5, 10]
-        # Minimum number of samples required at each leaf node
-        min_samples_leaf = [1, 2, 4]
-        # Method of selecting samples for training each tree
-        bootstrap = [True, False]
+        # max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+        # max_depth.append(None)
+        # # Minimum number of samples required to split a node
+        # min_samples_split = [2, 5, 10]
+        # # Minimum number of samples required at each leaf node
+        # min_samples_leaf = [1, 2, 4]
+        # # Method of selecting samples for training each tree
+        # bootstrap = [True, False]
         # Create the random grid
+
         random_grid = {'n_estimators': n_estimators,
-                       'max_features': max_features,
-                       'max_depth': max_depth,
-                       'min_samples_split': min_samples_split,
-                       'min_samples_leaf': min_samples_leaf,
-                       'bootstrap': bootstrap}
+                       'max_features': max_features}
+
+
+        # random_grid = {'n_estimators': n_estimators,
+        #                'max_features': max_features,
+        #                'max_depth': max_depth,
+        #                'min_samples_split': min_samples_split,
+        #                'min_samples_leaf': min_samples_leaf,
+        #                'bootstrap': bootstrap}
+
+
+
         pprint(random_grid)
 
 
@@ -83,10 +94,10 @@ class Model:
             #                                  min_impurity_decrease=self.min_impurity_decrease)
 
             rf = RandomForestRegressor()
-
+            rf.min_impurity_decrease =  self.min_impurity_decrease
             # Random search of parameters, using 3 fold cross validation,
             # search across 100 different combinations, and use all available cores
-            rf_random = RandomizedSearchCV(n_jobs=self.n_threads, estimator=rf, param_distributions=random_grid, n_iter=100, cv=3, verbose=2,
+            rf_random = RandomizedSearchCV(n_jobs=self.n_threads, estimator=rf, param_distributions=random_grid, cv=5, verbose=2,
                                            random_state=42)
             # Fit the random search model
             rf_random.fit(train_features, train_labels)
