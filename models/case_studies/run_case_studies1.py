40054e309e1df7ca124441dcb805674948eb7ca6
Weston Murdock <Murdock.Weston@epa.gov>
2026-05-26 15:00:11 -0400

    Added testing function for MTE to test all types of summary sheets at once

--- models/case_studies/run_case_studies1.py
+++ models/case_studies/run_case_studies1.py
@@ -12,13 +12,16 @@ from models.case_studies.run_model_building_db import run_dataset, ParametersGen
 
 from util import predict_constants as pc
 from model_ws_db_utilities import getEngine, getSession
-from models.ModelToExcel import ModelToExcel
+from models.ModelToExcel import ModelDataObjects, ModelToExcel
 import logging
 import json
 import pandas as pd
 from sqlalchemy import text
 import os
 from pathlib import Path
+
+load_dotenv("../../personal.env")
+PROJECT_ROOT = os.getenv("PROJECT_ROOT")
 
 def run_example():
     write_to_db = False
@@ -538,9 +541,74 @@ def run_rifm_rf_models():
     r = Results()
     r.summarize_model_stats(dataset_name, append_to_models_folder=append_to_models_folder)
 
+
+def full_test_mte():
+    # Need to test local and database models
+    # Need to test binary and continuous models
+    # Need to test models with and without an external set
+
+    write_to_db = False
+    append_to_models_folder = "_mte_testing"
+    ad_measure_model = [pc.Applicability_Domain_TEST_Embedding_Euclidean, pc.Applicability_Domain_TEST_Fragment_Counts]
+
+    # LOCAL/BINARY/NO EXTERNAL
+    dataset_name = "exp_prop_RBIODEG_301F v1 modeling"
+    run_dataset(dataset_name=dataset_name, qsar_method='gcm', feature_selection=False,
+                ad_measure_model=ad_measure_model, write_to_db=write_to_db,
+                append_to_models_folder=append_to_models_folder)  # OK
+
+    # LOCAL/BINARY/EXTERNAL
+    dataset_name = "exp_prop_RBIODEG_RIFM_CHEMREG"
+    run_dataset(dataset_name=dataset_name, qsar_method='gcm', feature_selection=False,
+                ad_measure_model=ad_measure_model, write_to_db=write_to_db,
+                append_to_models_folder=append_to_models_folder)  # OK
+
+    # LOCAL/CONTINUOUS/NO EXTERNAL
+    dataset_name = "HLC v1 modeling"
+    run_dataset(dataset_name=dataset_name, qsar_method='gcm', feature_selection=False,
+                ad_measure_model=ad_measure_model, write_to_db=write_to_db,
+                append_to_models_folder=append_to_models_folder)  # OK
+
+    # LOCAL/CONTINUOUS/EXTERNAL
+    dataset_name = "KOC v1 modeling"    
+    run_dataset(dataset_name=dataset_name, qsar_method='gcm', feature_selection=False,
+                ad_measure_model=ad_measure_model, write_to_db=write_to_db,
+                append_to_models_folder=append_to_models_folder)  # OK
+    
+    # DATABASE/BINARY/NO EXTERNAL
+    # Model id no longer exists in database
+    # model_id = 1567
+    # file_path = os.path.join(PROJECT_ROOT, "data", f"models{append_to_models_folder}", "database_models", f"continuous_no_external.xlsx")
+    # mdo = ModelDataObjects(model_id=model_id)
+    # mte = ModelToExcel(mdo, file_path)
+    # mte.create_excel()
+
+    # DATABASE/BINARY/EXTERNAL
+    model_id = 1831
+    file_path = os.path.join(PROJECT_ROOT, "data", f"models{append_to_models_folder}", "database_models", f"binary_external.xlsx")
+    mdo = ModelDataObjects(model_id=model_id)
+    mte = ModelToExcel(mdo, file_path)
+    mte.create_excel()
+
+    # DATABASE/CONTINUOUS/NO EXTERNAL
+    model_id = 1065
+    file_path = os.path.join(PROJECT_ROOT, "data", f"models{append_to_models_folder}", "database_models", f"continuous_no_external.xlsx")
+    mdo = ModelDataObjects(model_id=model_id)
+    mte = ModelToExcel(mdo, file_path)
+    mte.create_excel()
+
+    # DATABASE/CONTINUOUS/EXTERNAL
+    # No model id present in database yet
+    # model_id = 1567
+    # file_path = os.path.join(PROJECT_ROOT, "data", f"models{append_to_models_folder}", "database_models", f"continuous_no_external.xlsx")
+    # mdo = ModelDataObjects(model_id=model_id)
+    # mte = ModelToExcel(mdo, file_path)
+    # mte.create_excel()
+
+
 def main():
     
-    run_example()
+    # run_example()
     # run_Koc_knn_ga()
         
     # run_Koc()
@@ -549,7 +617,7 @@ def main():
     # run_biodeg_nite()
     
     # run_biodeg_rifm()
-    run_biodeg_301F()
+    # run_biodeg_301F()
      
     
     # run_pchem()
@@ -569,6 +637,8 @@ def main():
     # test_model_summary_local()
     # test_load_model_with_external_set()
     # run_rifm_rf_models()
+
+    full_test_mte()
 
 
 if __name__ == "__main__":
